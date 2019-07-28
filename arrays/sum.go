e5f15ff68a19563b0847d23dcec1e71800ab210c
Sanjay Singh Shekhawat <shekhawatsanjay23@gmail.com>
2019-07-28 17:37:44 +0530

    slices helper methods - make, copy, append

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,3 +8,31 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll adds all the slices and returns the total of slices in a new slice
+func SumAll(numbersToSum ...[]int) []int {
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SumAllTails adds all tails from given slices and returns a new slice
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,10 +2,11 @@ package arrays
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestSumSlices(t *testing.T) {
+func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -19,7 +20,53 @@ func TestSumSlices(t *testing.T) {
 
 }
 
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 5}, []int{2, 6})
+	want := []int{6, 8}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSum := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	}
+
+	t.Run("make the sum of all slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{2, 3, 5})
+		want := []int{5, 8}
+		checkSum(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{2, 3, 5})
+		want := []int{0, 8}
+		checkSum(t, got, want)
+	})
+}
+
 func ExampleSum() {
 	fmt.Println(Sum([]int{1, 2, 5, 3, 10}))
 	// Output: 21
+}
+
+func ExampleSumAll() {
+	fmt.Println(SumAll([]int{1, 2}, []int{3, 4}))
+	// Output: [3 7]
+}
+
+func ExampleSumAllTails() {
+	fmt.Println(SumAllTails([]int{1, 2, 5}, []int{2, 3, 10}))
+	fmt.Println(SumAllTails([]int{}, []int{2, 3, 11}))
+	// Output:
+	// [7 13]
+	// [0 14]
 }
